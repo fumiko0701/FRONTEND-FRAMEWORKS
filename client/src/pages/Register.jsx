029a9a9721dde16ab20a76552d60c44b0960b98d
Pedro Izidio <pedrosilvasilva879@gmail.com>
2025-11-23 17:50:58 -0300

    alteração no css

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/register.css";
+import "../styles/register_login.css";
 
 export default function Register() {
   const navigate = useNavigate();
